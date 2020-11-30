100b3820497f75837feb87ed1c8cf2753dea22e7
albenny hernandez <albennyh08@gmail.com>
2020-11-30 00:34:26 -0400

    haciendo la consulta de EntradProducto

---
+++ UI/Consultas/cEntradaProductos.xaml.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Proyecto_Final.BLL;
+using Proyecto_Final.Entidades;
+
+namespace Proyecto_Final.UI.Consultas
+{
+    /// <summary>
+    /// Interaction logic for cEntradaProductos.xaml
+    /// </summary>
+    public partial class cEntradaProductos : Window
+    {
+        public cEntradaProductos()
+        {
+            InitializeComponent();
+        }
+        private void ConsultarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<EntradaProductos>();
+
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0:
+                        try
+                        {
+                            listado = EntradaProductosBLL.GetList(u => u.EntradaProductoId == int.Parse(CriterioTextBox.Text));
+                        }
+                        catch (FormatException)
+                        {
+                            MessageBox.Show("Debes ingresar un Critero valido para aplicar este filtro.", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        }
+                        break;
+
+                    case 1:
+                        try
+                        {
+                            listado = EntradaProductosBLL.GetList(u => u.NombreProvedor.Contains(CriterioTextBox.Text));
+                        }
+                        catch (FormatException)
+                        {
+                            MessageBox.Show("Debes ingresar un Critero valido para aplicar este filtro.", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        }
+                        break;
+                    case 2:
+                        try
+                        {
+                            listado = EntradaProductosBLL.GetList(u => u.PrecioProducto==double.Parse(CriterioTextBox.Text));
+                        }
+                        catch (FormatException)
+                        {
+                            MessageBox.Show("Debes ingresar un Critero valido para aplicar este filtro.", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        }
+                        break;
+                    case 3:
+                        try
+                        {
+                            listado = EntradaProductosBLL.GetList(u => u.Cantidad==float.Parse(CriterioTextBox.Text));
+                        }
+                        catch (FormatException)
+                        {
+                            MessageBox.Show("Debes ingresar un Critero valido para aplicar este filtro.", "Advertencia", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                listado = EntradaProductosBLL.GetList(c => true);
+            }
+
+            if (DesdeDatePicker.SelectedDate != null)
+                listado = EntradaProductosBLL.GetList(c => c.FechaEntrada.Date >= DesdeDatePicker.SelectedDate);
+
+            if (HastaDatePicker.SelectedDate != null)
+                listado = EntradaProductosBLL.GetList(c => c.FechaEntrada.Date <= HastaDatePicker.SelectedDate);
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
+        }
+    }
+}
